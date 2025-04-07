ee3aea4146a1e94e8b3d02d5b3f037eb484c2405
sonu kumar <souravk03221@outlook.com>
2025-04-08 00:10:03 +0530

    added form file

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm:NgForm;
+  defaultQuestion = 'teacher';
+  answer='';
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form:NgForm){
+  //   console.log(form)
+  // }
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+}
