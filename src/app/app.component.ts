4ea2f6af4b64de940b46972c17728b618d304d25
sonu kumar <souravk03221@outlook.com>
2025-04-09 22:29:50 +0530

    outputing form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,15 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer='';
   genders = ['male','female'];
+  user = {
+    username:'',
+    email:'',
+    secretQuestion: '',
+    answer:'',
+    gender:''
+  }
+
+  submitted=false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -37,6 +46,13 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.signupForm);
+    // console.log(this.signupForm);
+    this.submitted= true;
+    this.user.username= this.signupForm.value.userData.username;
+    this.user.email= this.signupForm.value.userData.email;
+    this.user.secretQuestion= this.signupForm.value.secret;
+    this.user.answer= this.signupForm.value.questionAnswer;
+    this.user.gender= this.signupForm.value.gender;
+    this.signupForm.reset();
   }
 }
